76f9b613347efc463d13d65dc6f880297006c51e
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2023-08-06 12:17:51 -0300

    Commit Inicial

---
+++ ControleFluxo.java
@@ -0,0 +1,16 @@
+//FLUXO CONDICIONAL
+//Utilização do IF
+	
+public class ControleFluxo2 {
+	/*Método principal da classe*/
+	public static void main (String [] args) {
+		int idade = 8; //A variável idade é definida como 8 anos
+		if (idade < 10) { //Se a idade for menor que 10 anos
+			System.out.println ("Ainda é uma criança"); //Imprima em tela a mensagem
+		}
+	}//fim do método
+}//fim da classe
+
+/**
+o if indica para o programa que desejamos iniciar um teste. Assim, ele avaliará determinada condição entre parênteses (bloco). Caso essa condição retorne um valor verdadeiro (true), o programa executará as instruções definidas no código.
+**/
